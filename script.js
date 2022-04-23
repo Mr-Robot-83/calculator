a9feb24a8ef696aef12ecb056a84a134cc4fdc93
Mr-Robot-83 <scott.bonanno@gmail.com>
2022-04-23 22:32:47 +1000

    finish main functions

--- script.js
+++ script.js
@@ -2,50 +2,108 @@ const mainDisplay = document.querySelector(".main-display");
 const subDisplay = document.querySelector(".sub-display");
 const buttons = document.querySelectorAll(".buttons div");
 
-buttons.forEach(button => button.addEventListener('click', buttonClick))
-  
-let displayValue = [];
+buttons.forEach(button => button.addEventListener('click', buttonClick));
 
-function buttonClick(e){
-  switch (e.target.id) {
+
+let firstValue = [];
+let secondValue = [];
+let result = null;
+let operator = null;
+let operatorSymbol = null;
+
+//Monitor every button
+function buttonClick(e) {
+  switch(e.target.id) {
     case "clear":
-      subDisplay.textContent = "";
-      displayValue = [];
-    break;
+      clearMemory();
+      break;
     case "delete":
-      displayValue.pop();
-      let displayString = displayValue.join("")
-      subDisplay.textContent = displayString;    
+      backspace();
+      break;
+    case "+":
+    case "-":
+    case "/":
+    case "*":
+      updateOperator(e);
+      break;    
+    case "equals":
+      calculateResult();
       break;
     default:
-    updateDisplay(e)
-  }
-}
+    storeValues(e.target.id);
+  };
+  //Update the display after each keystroke
+  updateDisplay();
+};
 
-function updateDisplay(e){
-  console.log(e.target.id);
-  displayValue.push(e.target.id);
-  let displayString = displayValue.join("")
-  subDisplay.textContent = displayString;
-}
+function updateOperator (e) {
+  if(operator){return};
+  operator = e.target.id;
+  operatorSymbol = e.target.textContent;
+};
 
 
-function calculate(a, operator, b) {
+function backspace() {
+  if(result){clearMemory()};
+  if (secondValue[0]) {
+    secondValue.pop();
+  } else if (operator) {
+    operator = null;
+    operatorSymbol = null;
+  } else {
+    firstValue.pop();
+  };
+};
+
+//Check to see if there's an operator stored
+//Update first value if not, otherwise update the second value
+function storeValues(value){
+  if (result){clearMemory()};
+  if (!operator) {
+    firstValue.push(value);
+    console.log(firstValue.join(""))
+    } else {
+    secondValue.push(value); 
+    console.log(secondValue.join(""))
+  };
+};
+
+//Reset all stored values
+function clearMemory () {
+  subDisplay.textContent = "";
+  mainDisplay.textContent = "";
+  firstValue = [];
+  secondValue = [];
+  result = null;
+  operator = null;
+  operatorSymbol = null;
+};
+
+function calculateResult() {
+  let a = parseFloat(firstValue.join(""));
+  let b = parseFloat(secondValue.join(""));
   switch(operator) {
-    case "*":
-      console.log(a * b);
-      return a * b;
-    case "/":
-      console.log(a / b);
-      return a / b;
     case "+":
-      console.log(a + b);
-      return a + b;
+      result = a + b;
+      break;
     case "-":
-      console.log(a - b);
-      return a - b;  
-  }
-}
+      result = a - b;
+      break;
+    case "/":
+      result = a / b;
+      break;
+    case "*":
+      result = a * b;
+  };
+};
 
-calculate(9,"/",8);
+let displayString = "";
+let resultString = "";
 
+function updateDisplay(){
+  let symbol = "";
+  operatorSymbol ? symbol = ` ${operatorSymbol} ` : symbol = "";
+  displayString = firstValue.join("") + symbol + secondValue.join("");
+  subDisplay.textContent = displayString;
+  mainDisplay.textContent = result;
+};

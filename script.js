5cc52e0a7a253b8ccce10b86387c06efa65918fa
Mr-Robot-83 <scott.bonanno@gmail.com>
2022-04-24 00:51:31 +1000

    Keyboard support

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ const buttons = document.querySelectorAll(".buttons div");
 
 buttons.forEach(button => button.addEventListener('click', buttonClick));
 
-
 let firstValue = [];
 let secondValue = [];
 let result = null;
 let operator = null;
 let operatorSymbol = null;
 
-//Monitor every button
+//Monitor every button on the calc
 function buttonClick(e) {
   switch(e.target.id) {
     case "clear":
@@ -36,14 +35,18 @@ function buttonClick(e) {
   updateDisplay();
 };
 
-function updateOperator (e) {
-  if(operator){return};
+function updateOperator(e) {
+  //Stops the operator being changed mid sum
+  if(operator){return}; 
+  //Stops the operator being added if there's no first value
+  if(!firstValue[0]){return};
+  //Update the operator variables
   operator = e.target.id;
   operatorSymbol = e.target.textContent;
 };
 
-
 function backspace() {
+  //If the sum is complete, clear everything (same as the C button)
   if(result){clearMemory()};
   if (secondValue[0]) {
     secondValue.pop();
@@ -61,14 +64,12 @@ function storeValues(value){
   if (result){clearMemory()};
   if (!operator) {
     firstValue.push(value);
-    console.log(firstValue.join(""))
     } else {
     secondValue.push(value); 
-    console.log(secondValue.join(""))
   };
 };
 
-//Reset all stored values
+//Reset all stored values and clear the screen
 function clearMemory () {
   subDisplay.textContent = "";
   mainDisplay.textContent = "";
@@ -80,30 +81,84 @@ function clearMemory () {
 };
 
 function calculateResult() {
-  let a = parseFloat(firstValue.join(""));
-  let b = parseFloat(secondValue.join(""));
-  switch(operator) {
-    case "+":
-      result = a + b;
-      break;
-    case "-":
-      result = a - b;
-      break;
-    case "/":
-      result = a / b;
-      break;
-    case "*":
-      result = a * b;
+  if(firstValue[0] && secondValue[0]){
+    let a = parseFloat(firstValue.join(""));
+    let b = parseFloat(secondValue.join(""));
+    switch(operator) {
+      case "+":
+        result = a + b;
+        break;
+      case "-":
+        result = a - b;
+        break;
+      case "/":
+          if (b == 0){result = "MR.CALC SAYS NO";
+        } else {
+          result = a / b;
+        };
+        break;
+      case "*":
+        result = a * b;
+    };
   };
 };
 
-let displayString = "";
-let resultString = "";
-
 function updateDisplay(){
+  let displayString = "";
   let symbol = "";
-  operatorSymbol ? symbol = ` ${operatorSymbol} ` : symbol = "";
+  if(operatorSymbol){symbol = `\xa0${operatorSymbol}\xa0`};
   displayString = firstValue.join("") + symbol + secondValue.join("");
   subDisplay.textContent = displayString;
   mainDisplay.textContent = result;
+};
+
+//Keyboard support
+window.addEventListener('keydown', keystroke);
+
+function keystroke(e) {
+  console.log(e.key);
+  switch(e.key) {
+    case "C":
+    case "c":
+      clearMemory();
+      break;
+    case "Backspace":
+      backspace();
+      break;
+    case "+":
+    case "-":
+    case "/":
+    case "*":
+      updateOperatorKey(e.key);
+      break;    
+    case "=":
+    case "Enter":
+      calculateResult();
+      break;
+    case ".":
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      storeValues(e.key);
+      break;
+  };
+  //Update the display after each keystroke
+  updateDisplay();
+};
+
+function updateOperatorKey(e) {
+  //Stops the operator being changed mid sum
+  if(operator){return}; 
+  //Stops the operator being added if there's no first value
+  if(!firstValue[0]){return};
+  //Update the operator variables
+  operator = e;
+  operatorSymbol = e;
 };
